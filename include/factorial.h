878827db2bba30344031a9f56883fdaab4a4ed03
acrowe03 <acrowe03@uoguelph.ca>
2017-09-26 14:17:51 -0400

    formatting of the files

---
+++ include/factorial.h
@@ -0,0 +1,7 @@
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+//originally in factorial.h
+unsigned long long int fact(int n);
+
+#endif
---
+++ include/matrices.h
@@ -0,0 +1,9 @@
+#ifndef MATRICES_H
+#define MATRICES_H
+
+//originally in matrices.h
+void matrix_sum();
+void matrix_product();
+void matrix_transpose();
+
+#endif
