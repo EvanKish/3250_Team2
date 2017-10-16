fecf4f60b41fea0ddc20beebbeb686963d3492bc
acrowe03 <acrowe03@uoguelph.ca>
2017-10-16 17:20:58 -0400

    Refactored

--- src/factorial.c
+++ src/factorial.c
@@ -1,14 +1,18 @@
 #include "definitions.h"
-#include "factorial.h"  
-unsigned long long int fact(int n){/*Factorial function inefficient for values after 20*/
-    unsigned long long int fact;
-    int i;
-    fact=1;
-
-    for(i=n;i>0;i--){
-        fact*=i;
-    }
-    return fact;
-    }
+#include "factorial.h"
 
 
+/**
+ *This function returns the factorial of a number
+ *Factorial function inefficient for values after 20
+ *@param n number to be computed
+ *@return factorial of n
+ */
+unsigned long long int fact(int n) {
+    unsigned long long int factorial = 1;
+
+    for (int i=n;i>0;i--)
+        factorial*=i;
+
+    return factorial;
+}
