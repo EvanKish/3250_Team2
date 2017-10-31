ca40e4c42a45aa3682fe3f19704c6ddbf04b0e4c
tejedu <awoh.g17@gmail.com>
2017-10-31 14:45:21 -0400

    Created functions for new matrix struct, including inputMatrix, rewrote calculator functions to use new functionality

--- include/matrices.h
+++ include/matrices.h
@@ -8,6 +8,57 @@
 #define MATRICES_H
 
 /**
+ * Stores the size and data of a matrix
+ */
+typedef struct matrix {
+    int rows;
+    int cols;
+    double** cells;
+} Matrix;
+
+/**
+ * Creates a new matrix of that size
+ * @param rows Number of rows
+ * @param cols Number of cols
+ */
+Matrix* createMatrix(int rows, int cols);
+
+/**
+ * Deletes a matrix (frees the memory)
+ * @param matrix The matrix to delete
+ */
+void deleteMatrix(Matrix* matrix);
+
+/**
+ * Creates a matrix through user input
+ * @param rows Number of rows
+ * @param cols Number of cols
+ */
+Matrix* inputMatrix(int rows, int cols);
+
+/**
+ * Returns the cell of a matrix at a specific position
+ * @param matrix The matrix we're talking about
+ * @param row The row to return from
+ * @param col The col to return from
+ */
+double matrixGetCell(Matrix* matrix, int row, int col);
+
+/**
+ * Sets a cell of a matrix at a specific position
+ * @param row The row to set
+ * @param col The col to set
+ * @param value The value to update to
+ */
+void matrixSetCell(Matrix* matrix, int row, int col, double value);
+
+/**
+ * Prints a matrix
+ * @param matrix Matrix to print
+ */
+void printMatrix(Matrix* matrix);
+
+/**
  * Sums two matrices
  * Takes input from user on stdin
  * Prints output to stdout
@@ -27,5 +78,21 @@ void matrixProduct();
  * Prints output to stdout
  */
 void matrixTranspose();
+
+/**
+ * Scans one integer safely and repeats
+ * the message until it receives it.
+ * @param message Prompt for the input
+ * @param tryAgainMessage Prompt for the input after the first try, empty string "" means use first message
+ */
+int scanSafeInt(char* message, char* tryAgainMessage);
+
+/**
+ * Scans one double safely and repeats
+ * the message until it receives it.
+ * @param message Prompt for the input
+ * @param tryAgainMessage Prompt for the input after the first try, empty string "" means use first message
+ */
+double scanSafeDouble(char* message, char* tryAgainMessage);
 
 #endif
--- src/matrices.c
+++ src/matrices.c
@@ -5,137 +5,217 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "matrices.h"
 
-void matrixSum() {
-    int matrix1Rows, matrix1Cols, matrix2Rows, matrix2Cols;
+Matrix* createMatrix(int rows, int cols) {
+    Matrix* matrix = malloc(sizeof(Matrix));
+    matrix->cells = malloc(sizeof(double*) * rows);
     
-    printf("Enter the number of rows of matrix 1:");
-    scanf("%d", &matrix1Rows);
-    printf("Enter the number of columns of matrix :");
-    scanf("%d", &matrix1Cols);
-    printf("Enter the number of rows of matrix 2:");
-    scanf("%d", &matrix2Rows);
-    printf("Enter the number of columns of matrix 2:");
-    scanf("%d", &matrix2Cols);
-    
-    if(matrix1Rows == matrix2Rows && matrix1Cols == matrix2Cols) {
-        float matrix1[matrix1Rows][matrix1Cols];
-        float matrix2[matrix2Rows][matrix2Cols];
-        float matrixResult[matrix1Rows][matrix1Cols];
-
-        for(int i = 0; i < matrix1Rows; i++) {
-            printf("Enter the members of matrix 1 row %d :", i + 1);
-            
-            for(int j = 0; j < matrix1Cols; j++) {
-                scanf("%f", &matrix1[i][j]);
-            }
-        }
-        
-        for(int i = 0; i < matrix2Rows; i++) {
-            printf("Enter the members of matrix 2 row %d :", i + 1);
-            
-            for(int j = 0; j < matrix2Cols; j++) {
-                scanf("%f", &matrix2[i][j]);
-            }
-        }
-        
-        for(int i = 0; i < matrix1Rows; i++) {
-            for(int j = 0; j < matrix1Cols; j++) {
-                matrixResult[i][j] = matrix1[i][j] + matrix2[i][j];
-            }
-        }
-        
-        printf("The sum of both matrices is\n:");
-        
-        for(int j = 0; j < matrix1Cols; j++) {
-            for(int i = 0; i < matrix1Rows; i++) {
-                printf("\t\t %.0f", matrixResult[i][j]);
-            }
-            
-            printf("\n");
-        }
-    } else {
-        printf("\n\tThe matrices are incompatible");
+    for(int row = 0; row < rows; row++) {
+        matrix->cells[row] = malloc(sizeof(double) * cols);
     }
+    
+    matrix->cols = cols;
+    matrix->rows = rows;
+    return matrix;
+}
+
+void deleteMatrix(Matrix* matrix) {
+    for(int row = 0; row < matrix->rows; row++) {
+        free(matrix->cells[row]);
+    }
+    
+    free(matrix->cells);
+    free(matrix);
+}
+
+Matrix* inputMatrix(int rows, int cols) {
+    Matrix* matrix = createMatrix(rows, cols);
+    
+    printf("Input matrix:\n");
+    
+    for(int x = 0; x < rows; x++) {
+        printf("Enter the members of row %d:\n", x + 1);
+        
+        for(int y = 0; y < cols; y++) {
+            printf("Member %d\n", y + 1);
+            matrixSetCell(matrix, x, y, scanSafeDouble(": ", "Try again: "));
+        }
+    }
+    
+    return matrix;
+}
+
+double matrixGetCell(Matrix* matrix, int row, int col) {
+    double value = 0;
+    
+    if(row >= 0 && col >= 0 && row < matrix->rows && col < matrix->cols) {
+        value = matrix->cells[row][col];
+    } else {
+        printf("ERROR: Matrix out of bounds get.\n");
+    }
+    
+    return value;
+}
+
+void matrixSetCell(Matrix* matrix, int row, int col, double value) {
+    if(row >= 0 && col >= 0 && row < matrix->rows && col < matrix->cols) {
+        matrix->cells[row][col] = value;
+    } else {
+        printf("ERROR: Matrix out of bounds set.\n");
+    }
+}
+
+void printMatrix(Matrix* matrix) {
+    for(int row = 0; row < matrix->rows; row++) {
+        for(int col = 0; col < matrix->cols; col++) {
+            printf("%4.1lf ", matrix->cells[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+void matrixSum() {
+    int matrix1Rows;
+    int matrix1Cols;
+    int matrix2Rows;
+    int matrix2Cols;
+    
+    matrix1Rows = scanSafeInt("Enter the number of rows of matrix 1: ", "");
+    matrix1Cols = scanSafeInt("Enter the number of columns of matrix 1: ", "");
+    matrix2Rows = scanSafeInt("Enter the number of rows of matrix 2: ", "");
+    matrix2Cols = scanSafeInt("Enter the number of columns of matrix 2: ", "");
+        
+    if(!(matrix1Rows == matrix2Rows && matrix1Cols == matrix2Cols)) {
+        printf("The matrices are incompatible.");
+        return;
+    }
+    
+    Matrix* matrix1 = inputMatrix(matrix1Rows, matrix1Cols);
+    Matrix* matrix2 = inputMatrix(matrix1Rows, matrix1Cols);
+    Matrix* matrixResult = createMatrix(matrix1Rows, matrix1Cols);
+    
+    for(int row = 0; row < matrixResult->rows; row++) {
+        for(int col = 0; col < matrixResult->cols; col++) {
+            matrixSetCell(matrixResult, row, col, matrixGetCell(matrix1, row, col) + matrixGetCell(matrix2, row, col));
+        }
+    }
+    
+    printMatrix(matrixResult);
+    
+    deleteMatrix(matrix1);
+    deleteMatrix(matrix2);
+    deleteMatrix(matrixResult);
 }
 
 void matrixProduct() {
-    int matrix1Rows, matrix1Cols, matrix2Rows, matrix2Cols;
+    int matrix1Rows;
+    int matrix1Cols;
+    int matrix2Rows;
+    int matrix2Cols;
     
-    printf("Enter the number of rows of matrix 1:");
-    scanf("%d", &matrix1Rows);
-    printf("Enter the number of columns of matrix :");
-    scanf("%d", &matrix1Cols);
-    printf("Enter the number of rows of matrix 2:");
-    scanf("%d", &matrix2Rows);
-    printf("Enter the number of columns of matrix 2:");
-    scanf("%d", &matrix2Cols);
+    matrix1Rows = scanSafeInt("Enter the number of rows of matrix 1: ", "");
+    matrix1Cols = scanSafeInt("Enter the number of columns of matrix 1: ", "");
+    matrix2Rows = scanSafeInt("Enter the number of rows of matrix 2: ", "");
+    matrix2Cols = scanSafeInt("Enter the number of columns of matrix 2: ", "");
     
-    float matrix1[matrix1Rows][matrix1Cols];
-    float matrix2[matrix2Rows][matrix2Cols];
-    float matrixResult[matrix1Rows][matrix1Cols];
+    if(!(matrix1Rows == matrix2Cols)) {
+        printf("The matrices are incompatible.");
+        return;
+    }
     
-    for(int i = 0; i < matrix1Rows; i++) {
-        printf("Enter the members of matrix 1 row %d :", i + 1);
-        
-        for(int j = 0; j < matrix1Cols; j++) {
-            scanf("%f", &matrix1[i][j]);
+    Matrix* matrix1 = inputMatrix(matrix1Rows, matrix1Cols);
+    Matrix* matrix2 = inputMatrix(matrix2Rows, matrix2Cols);
+    Matrix* matrixResult = createMatrix(matrix1Rows, matrix2Cols);
+    int terms = matrix1->cols;
+    
+    for(int row = 0; row < matrixResult->rows; row++) {
+        for(int col = 0; col < matrixResult->cols; col++) {
+            double product = 0;
+            
+            for(int i = 0; i < terms; i++) {
+                product +=
+                    matrixGetCell(matrix1, row, i) *
+                    matrixGetCell(matrix2, i, col);
+            }
+            
+            matrixSetCell(matrixResult, row, col, product);
         }
     }
     
-    for(int i = 0; i < matrix2Rows; i++) {
-        printf("Enter the members of matrix 2 row %d :", i + 1);
-        
-        for(int j = 0; j < matrix2Cols; j++) {
-            scanf("%f", &matrix2[i][j]);
-        }
-    }
+    printMatrix(matrixResult);
     
-    //matrixResult[i][j] = 1;
-    
-    for(int i = 0;i < matrix1Rows; i++) {
-        for(int j = 0; j < matrix2Cols; j++) {
-            matrixResult[i][j] = 0;
-            matrixResult[i][j] += matrix1[i][j] * matrix2[i][j];
-        }
-    }
+    deleteMatrix(matrix1);
+    deleteMatrix(matrix2);
+    deleteMatrix(matrixResult);
+}
 
-    printf("The  of the matrix is\n:");
+void matrixTranspose() {
+    int matrixRows;
+    int matrixCols;
     
-    for(int j = 0; j < matrix1Cols; j++) {
-        for(int i = 0; i < matrix2Rows; i++) {
-            printf("\t\t %.0f", matrix1[i][j]);
+    matrixRows = scanSafeInt("Enter the number of rows of the matrix: ", "");
+    matrixCols = scanSafeInt("Enter the number of columns of the matrix: ", "");
+    
+    Matrix* matrix = inputMatrix(matrixRows, matrixCols);
+    
+    printf("The transposed matrix is: \n");
+    
+    for(int col = 0; col < matrix->cols; col++) {
+        for(int row = 0; row < matrix->rows; row++) {
+            printf("%4.1lf ", matrix->cells[row][col]);
         }
-        
         printf("\n");
     }
 }
 
-void matrixTranspose() {
-    int matrixRows, matrixCols;
+int scanSafeInt(char* message, char* tryAgainMessage) {
+    int success = 0;
+    int number = 0;
+    int attempts = 0;
     
-    printf("Enter the number of rows:");
-    scanf("%d", &matrixRows);
-    printf("Enter the number of columns:");
-    scanf("%d", &matrixCols);
-    
-    float matrix[matrixRows][matrixCols];
-    
-    for(int i = 0; i < matrixRows; i++) {
-        printf("Enter the members of row %d :", i + 1);
-        
-        for(int j = 0; j < matrixCols; j++) {
-            scanf("%f", &matrix[i][j]);
+    while(!success) {
+        if(attempts == 0 || tryAgainMessage[0] == '\0') {
+            printf(message);
+        } else {
+            printf(tryAgainMessage);
         }
+        
+        success = scanf("%d", &number);
+        
+        if(!success) {
+            char trash[40];
+            scanf("%s", trash);
+        }
+        
+        attempts++;
     }
+    
+    return number;
+}
 
-    printf("The transpose of the matrix is\n:");
-    for(int j = 0; j < matrixCols; j++) {
-        for(int i = 0; i < matrixRows; i++) {
-            printf("\t\t %.0f", matrix[i][j]);
+double scanSafeDouble(char* message, char* tryAgainMessage) {
+    int success = 0;
+    double number = 0;
+    int attempts = 0;
+    
+    while(!success) {
+        if(attempts == 0 || tryAgainMessage[0] == '\0') {
+            printf(message);
+        } else {
+            printf(tryAgainMessage);
         }
         
-        printf("\n");
-     }
+        success = scanf("%lf", &number);
+        
+        if(!success) {
+            char trash[40];
+            scanf("%s", trash);
+        }
+        
+        attempts++;
+    }
+    
+    return number;
 }
